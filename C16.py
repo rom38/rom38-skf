7004ccf9cfac7793a51428109db5dbca72915fb6
Rman vsc <rom38@users.noreply.github.com>
2022-08-13 15:07:00 +0000

    OOP

--- C16.py
+++ C16.py
@@ -39,3 +39,49 @@ for event in events:
     event_obj = Event()
     event_obj.init_from_dict(event)
     print(event_obj.timestamp)
+# %%
+class Room1:
+   def get_room(self):
+       print('room1')
+ 
+class Room2:
+   def get_room(self):
+       print('room2')
+ 
+   def get_room2(self):
+       print('room2 for flat')
+ 
+class Kitchen:
+   def get_kitchen(self):
+       print('kitchen')
+ 
+class Flat(Kitchen,Room1,Room2):
+   ...
+ 
+f=Flat()
+f.get_kitchen()
+f.get_room()
+f.get_room2()
+# %%
+class Room:
+   def get_room(self):
+       print('room')
+ 
+class Room1(Room):
+   def get_room(self):
+       print('room1')
+ 
+class Room2(Room):
+   def get_room(self):
+       print('room2')
+ 
+ 
+ 
+class Flat(Room1,Room2):
+   ...
+ 
+print(Flat.mro()) #  метод класса, который показывает порядок наследования
+ 
+f=Flat()
+f.get_room()
+# %%
---
+++ testCat.py
@@ -0,0 +1,10 @@
+# %%
+from Cat import Cat
+
+sam = Cat('Сэм','мальчик',2)
+print (f'Имя: {sam.get_name()}, Пол: {sam.get_sex()}, Возраст: {sam.get_age()} года')
+# %%
+from Cat import Dog
+dog_1 = Dog('Мухтар','мальчик',0)
+print(dog_1.get_pet())
+# %%
