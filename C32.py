f24831074003e18de3d17ddde33c593fe09fdbe0
Rman vsc <rom38@users.noreply.github.com>
2022-08-22 08:50:48 +0000

    oop 3

--- C32.py
+++ C32.py
@@ -1,3 +1,5 @@
+# %%
+
 class ParentException(Exception):  # создаём пустой класс – исключения потомка, наследуемся от exception
     pass
 
@@ -10,3 +12,28 @@ try:
     raise ChildException("message")  # поднимаем исключение-наследник
 except ParentException as e:  # ловим его родителя
     print(e)  # выводим информацию об исключении
+
+# %%
+# 3.2.5
+class Square():
+    def __init__(self, side):
+        if side <= 0:
+            raise NonPositiveDigitException
+        self.side = side
+
+
+class SquareFactory():
+    @staticmethod
+    def side_square(side):
+        return Square(side)
+
+class NonPositiveDigitException(ValueError):
+    pass
+
+
+# sq = SquareFactory.side_square(7)
+sq = Square(-5)
+print(sq)
+print(sq.side)
+
+# %%
---
+++ C33.py
@@ -0,0 +1,13 @@
+# %%
+# 3.3.6
+
+
+import time
+
+i = 10
+while i != -1:
+    print(i)
+    time.sleep(1)
+    i -= 1
+print('время вышло')
+# %%
